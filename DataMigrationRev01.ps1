00c12d0afbbabbacf6eed82afa54b8c836ce3420
gmotardz <95711326+gmotardz@users.noreply.github.com>
2021-12-09 09:31:38 -0800

    Add files via upload
    work in progress.. has not been fully tested.

---
+++ DataMigrationRev01.ps1
@@ -0,0 +1,9 @@
+<#>
+Work in progress. Has not been fully tested. Might change the cmdlet to robocopy due to permissions. 
+</#>
+
+$folderName = "$env:COMPUTERNAME-"+(Get-Date –f "MM-dd-yyyy")
+$Destination = New-Item -ItemType Directory -Path insert destination path here -Name $folderName
+
+Copy-Item -Path insert target path here -Destination $Destination -Recurse
+
