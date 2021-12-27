22f06347672a2af64034fb748214ada1bd0d4347
gmotardz <95711326+gmotardz@users.noreply.github.com>
2021-12-27 14:23:23 -0800

    Update LocalAdminPWReset.ps1

--- LocalAdminPWReset.ps1
+++ LocalAdminPWReset.ps1
@@ -1,6 +1,6 @@
 <#>
-Make sure to define the new PW and the user account
+Make sure to define the new PW and the user account also make sure elevated powershell console is running. 
 </#>
 
-$NewPW = ConvertTo-SecureString "INSERTNEWPWHERE" –AsPlainText –Force
+$NewPW = ConvertTo-SecureString "INSERTNEWPWHERE" –AsPlainText –Force 
 Set-LocalUser –Name INSERTUSERNAME –Password $NewPW  
