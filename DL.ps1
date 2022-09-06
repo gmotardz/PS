2c8f3e5f62e71a9a59bdbbda577d2cb878dfe704
gmotardz <95711326+gmotardz@users.noreply.github.com>
2022-09-06 14:13:44 -0700

    Add files via upload

---
+++ DL.ps1
@@ -0,0 +1,17 @@
+﻿
+do{
+    try{
+        $DL = Read-Host "Enter DL"
+        $list_users = @{N="Email";E={$_mail.ToLower()}} 
+        $list_users = Get-ADGroupMember -Identity "$DL" -Recursive |
+        Get-ADUser -Properties Mail |
+        Select-Object Mail 
+        $list_users | Export-CSV -Path .\file1.csv -notypeinformation 
+        (Get-Content .\file1.csv).ToLower() | out-file .\file1.csv
+        (Import-Csv .\file1.csv).Mail -replace '^.*$', '"$&"' -join "," > .\file2.txt
+        code .\file2.txt
+        }
+    catch {Write-Warning "$DL does not exist, please input a valid Distribution List."}
+}   until ($list_users) 
+
+Write-Host "File created, copy contents."
